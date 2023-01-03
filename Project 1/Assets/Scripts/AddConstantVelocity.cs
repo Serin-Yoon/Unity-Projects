ae90b690679192309a42c4ef9ff76d421be9dcf6
Serin-Yoon <bcode429@gmail.com>
2023-01-03 19:09:41 +0900

    upload Project 1

---
+++ Project 1/Assets/Scripts/AddConstantVelocity.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody>().velocity += v3Force;
+    }
+}
---
+++ Project 1/Assets/Scripts/AddPlayerControlledVelocity.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerControlledVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+
+    [SerializeField]
+    KeyCode keyPositive;
+    [SerializeField]
+    KeyCode keyNegative;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Force;
+
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Force;
+     }
+}
